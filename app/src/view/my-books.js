b007a81a8a28d3722637fa340658dcec4e302b20
alisa-fr <86801044+alisa-fr@users.noreply.github.com>
2022-09-30 09:47:26 +0200

    Dev (#45)
    * Basic HTML
    
    * first steps with appwrite
    
    * added team members info to readme
    
    * Merge UI with dev (#35)
    
    * Change source branch for deployment workflow
    
    * Update UI
    
    Co-authored-by: Alexander Bazo <alexanderbazo@googlemail.com>
    Co-authored-by: TrashCoon <eliph0500@gmail.com>
    
    * login/register with appwrite, better page handling (#36)
    
    * Appwrite (#37)
    
    * login/register with appwrite, better page handling
    
    * update links and add pages to index
    
    * Appwrite (#38)
    
    * profile update possible #1
    
    * some design adjustments
    
    * Appwrite (#39)
    
    * profile update possible #1
    
    * some design adjustments
    
    * bug fix
    
    * Added missing elements + started debugging
    
    * Finished Debugging
    
    * Adjusting UI
    
    * Added search filter options
    
    * Adjustments
    
    * Fixed feed bug
    
    * Appwrite (#41)
    
    * #18
    
    * ui changes
    
    * #3
     
    * #7
    
    * #8 
    
    * #10 
    
    * #15 
    
    * #24
    
    * #25
    
    * #32
    
    * #6 #11 #12 #16 #19 #20 #22 #23 #26 #28 #29 #33 (#42)
    
    * Chat (#44)
    
    * #6 #11 #12 #16 #19 #20 #22 #23 #26 #28 #29 #33
    
    * #30 #31
    
    Co-authored-by: TrashCoon <eliph0500@gmail.com>
    Co-authored-by: Lianna <82600042+liannaaleks@users.noreply.github.com>
    Co-authored-by: Alexander Bazo <alexanderbazo@googlemail.com>
    Co-authored-by: TrashCoon <76110797+TrashCoon@users.noreply.github.com>

--- app/src/view/my-books.js
+++ app/src/view/my-books.js
@@ -1,8 +1,26 @@
 import {searchBooks} from "../services/google-book-api.js";
-import {loadMyBookList} from "../services/my-books.js";
+import {loadMyBookList, loadTradedList} from "../services/my-books.js";
 import {getBookTemplate} from "../view/book.js";
 
 export function myBooksLoadUserData () {
+
+    loadTradedList().then(e => {
+
+        const ownedListEl = document.getElementById("traded-book-list");
+        ownedListEl.innerHTML = "";
+
+        e.documents.forEach(book => {
+
+            searchBooks("isbn:"+book.isbn, (result) => {
+
+                let bookItem = result.items[0].volumeInfo;
+                const bookEl = getBookTemplate(bookItem);
+                ownedListEl.append(bookEl);
+            });
+
+        });
+
+    });
 
     loadMyBookList().then(e => {
 
@@ -17,8 +35,8 @@ export function myBooksLoadUserData () {
                 const bookEl = getBookTemplate(bookItem);
                 ownedListEl.append(bookEl);
                 bookEl.querySelector(".actions").innerHTML =
-                    `<button class='btn' onclick='removeBookFromMyBookListHandler("${book.$id}", "${book.isbn}")'><i class='fa fa-minus' aria-hidden='true'></i></button>`;
-
+                    `<button class='btn' onclick='removeBookFromMyBookListHandler("${book.$id}", "${book.isbn}")'><i class='fa fa-minus' aria-hidden='true'></i></button>` + 
+                    `<button title="Buch getauscht" class='btn' onclick='changeBookStateToTradedHandler("${book.$id}", "${book.isbn}")'><i class='fa fa-exchange' aria-hidden='true'></i></button>`;
             });
 
         });
